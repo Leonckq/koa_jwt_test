aa15f5baa608bca97efd03bafc493838df46c667
leon <liyang@hosecloud.com>
2020-10-23 20:46:06 +0800

    feat: 404和错误页

---
+++ routes/users.js
@@ -0,0 +1,55 @@
+const router = require('koa-router')()
+const jwt = require('jsonwebtoken')
+const util = require('util')
+const verify = util.promisify(jwt.verify)
+const { SECRET } = require('../conf/constants')
+router.prefix('/users')
+
+router.get('/', function (ctx, next) {
+  ctx.body = 'this is a users response!'
+})
+
+router.get('/bar', function (ctx, next) {
+  ctx.body = 'this is a users/bar response'
+})
+
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body
+  let userInfo, token
+  if (userName === 'zhangsan' && password === 'abc') {
+    userInfo = {
+      userId: 1,
+      userName: 'zhangsan',
+      nickName: '张三',
+      gender: 1 // 男
+    }
+    token = jwt.sign(userInfo, SECRET, {
+      expiresIn: '1h'
+    })
+  }
+  if(userInfo == null) {
+    ctx.body = {
+      errno: -1,
+      msg: '登录失败'
+    }
+    return
+  }
+  ctx.body = {
+    errno: 0,
+    data: token
+  }
+})
+
+// 获取用户信息
+router.get('/getUserInfo', async(ctx, next) => {
+  const token = ctx.header.authorization
+  let userInfo
+  try {
+    const payload = await verify(token.split(' ')[1], SECRET)
+    ctx.body = {errno: 0, data: payload }
+  } catch (error) {
+    ctx.body = {errno: -1, data: 'verify error' }
+  }
+})
+
+module.exports = router
